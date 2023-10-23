b7ec0d1c4c6d9589f8b365d54cd8a304f89d5491
Gwenitora <gwenitora.gardu74@gmail.com>
2023-10-23 13:14:14 +0200

    update Table (bugged)

--- table.cpp
+++ table.cpp
@@ -1,6 +1,16 @@
 #include "cell.h"
+#include <iostream>
+//using namespace std;
 
 class Table
 {
-	
+	std::vector<list<Cell>> _Cells;
+	int _sizeX;
+	int _sizeY;
+
+	Table(int sizeX, int sizeY)
+	{
+		_sizeX = sizeX;
+		_sizeY = sizeY;
+	}
 };
