4fff0789c06377f7d2cadb1d1fd269bd2bb0035e
Maquereauu <macrorush@gmail.com>
2023-10-31 17:13:51 +0100

    update : sdl 👍

--- window.h
+++ window.h
@@ -1,7 +1,22 @@
 #pragma once
+
 #include <SDL.h>
+#include <vector>
+#include "gameObject.h"
+
+using namespace std;
+
 class Window
 {
-	SDL_Surface* winSurface;
-	SDL_Window* window;
+public:
+	int _sizeX;
+	int _sizeY;
+	vector<GameObject> _objectList;
+	SDL_Window* _window;
+	SDL_Renderer* _renderer;
+	vector<vector<SDL_Rect>> _grid;
+	Window(int sizeX,int sizeY);
+	Window();
+	void Draw();
+	void DrawGrid();
 };
