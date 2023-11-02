9c851e133fc70b5294709968e8c5a94ea549387a
Maquereau2 <macrorush@gmail.com>
2023-11-02 22:07:15 +0100

    update : 👌

--- 2048.cpp
+++ 2048.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include "game.h"
-#include "graphic.h"
-#include "window.h"
 
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
-	//Graphic _graphic;
-	//Window _window;
     Game _game;
 
 	return 0;
